0ebec20aa5ca945dd7681f138de8f1ebcfe7c2aa
lucasglockshuber <lucas.glockshuber@outlook.com>
2020-09-28 10:55:01 -0300

    commit all

---
+++ ObrasLiterarias/Context/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ObrasLiterarias.Model;
+
+namespace ObrasLiterarias.Context
+{
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            { 
+            }
+            public DbSet<Autor> Autores { get; set; }
+            protected override void OnModelCreating(ModelBuilder builder)
+            {
+                builder.Entity<Autor>().HasKey(m => m.Id);
+                base.OnModelCreating(builder);
+            }
+    }
+}
+
+
+
---
+++ ObrasLiterarias/Migrations/20200925003820_InitialCreate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ObrasLiterarias.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autores", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Autores");
+        }
+    }
+}
